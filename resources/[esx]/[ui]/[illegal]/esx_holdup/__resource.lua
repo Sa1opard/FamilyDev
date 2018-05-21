72738966f85586ac370d4e7f22368c5879bc8847
Sa1opard <38191241+Sa1opard@users.noreply.github.com>
2018-05-21 19:10:16 +0200

    Add files via upload

---
+++ resources/[esx]/[ui]/[illegal]/esx_holdup/__resource.lua
@@ -0,0 +1,24 @@
+version '1.0.1'
+
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'locales/de.lua',
+    'locales/br.lua',
+    'locales/en.lua',
+    'locales/fr.lua',
+    'locales/sv.lua',
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'locales/de.lua',
+    'locales/br.lua',
+    'locales/en.lua',
+    'locales/fr.lua',
+    'locales/sv.lua',
+    'config.lua',
+    'server/main.lua'
+}
---
+++ resources/[esx]/[ui]/[illegal]/esx_holdup/config.lua
@@ -0,0 +1,164 @@
+Config = {}
+Config.Locale = 'fr'
+
+Config.PoliceNumberRequired = 2
+Config.TimerBeforeNewRob = 1800 -- seconds
+
+
+-- Change secondsRemaining if you want another timer
+Stores = {
+    --[["paleto_twentyfourseven"] = {
+        position = { ['x'] = 1736.32092285156, ['y'] = 6419.4970703125, ['z'] = 35.037223815918 },
+        reward = math.random(100,1000),
+        nameofstore = "24/7. (Paleto Bay)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },]]
+    ["sandyshores_twentyfoursever"] = {
+        position = { ['x'] = 1961.24682617188, ['y'] = 3749.46069335938, ['z'] = 32.3437461853027 },
+        reward = math.random(500,2500),
+        nameofstore = "24/7. (Sandy Shores)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["bar_one"] = {
+        position = { ['x'] = 1982.5084228516, ['y'] = 3053.4812011719, ['z'] = 47.215114593506 },
+        reward = math.random(500,2500),
+        nameofstore = "Yellow Jack. (Sandy Shores)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["ocean_liquor"] = {
+        position = { ['x'] = -2959.33715820313, ['y'] = 388.214172363281, ['z'] = 14.0432071685791 },
+        reward = math.random(1000,3000),
+        nameofstore = "Robs Liquor. (Great Ocean Higway)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["sanandreas_liquor"] = {
+        position = { ['x'] = -1219.85607910156, ['y'] = -916.276550292969, ['z'] = 11.3262157440186 },
+        reward = math.random(3000,8000),
+        nameofstore = "Robs Liquor. (San andreas Avenue)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["grove_ltd"] = {
+        position = { ['x'] = -43.4035377502441, ['y'] = -1749.20922851563, ['z'] = 29.421012878418 },
+        reward = math.random(3000,8000),
+        nameofstore = "LTD Gasoline. (Grove Street)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["mirror_ltd"] = {
+        position = { ['x'] = 1160.67578125, ['y'] = -314.400451660156, ['z'] = 69.2050552368164 },
+        reward = math.random(3000,8000),
+        nameofstore = "LTD Gasoline. (Mirror Park Boulevard)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["clinton"] = {
+        position = { ['x'] = 379.946685, ['y'] = 332.572845, ['z'] = 103.56644 },
+        reward = math.random(500,2500),
+        nameofstore = "Clinton Ave",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["route15"] = {
+        position = { ['x'] = 2549.871826, ['y'] = 386.79299, ['z'] = 108.623039 },
+        reward = math.random(500,2500),
+        nameofstore = "le magasin Route 15",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["inseno"] = {
+        position = { ['x'] = -3047.9531, ['y'] = 587.646118, ['z'] = 7.908931 },
+        reward = math.random(500,2500),
+        nameofstore = "Inseno Rd",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["barbereno"] = {
+        position = { ['x'] = -3249.3244, ['y'] = 1006.405700, ['z'] = 12.830717 },
+        reward = math.random(500,2500),
+        nameofstore = "Barbereno Rd",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["route68"] = {
+        position = { ['x'] = 544.454406, ['y'] = 2662.963134, ['z'] = 42.156528 },
+        reward = math.random(500,2500),
+        nameofstore = "le magasin Route 68",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["route13"] = {
+        position = { ['x'] = 2674.514648, ['y'] = 3288.175781, ['z'] = 55.24114 },
+        reward = math.random(500,2500),
+        nameofstore = "le magasin Route 13",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["bar_one"] = {
+        position = { ['x'] = 1982.5084228516, ['y'] = 3053.4812011719, ['z'] = 47.215114593506 },
+        reward = math.random(500,2500),
+        nameofstore = "Yellow Jack. (Sandy Shores)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["greatocean"] = {
+        position = { ['x'] = 1736.51855, ['y'] = 6419.45507, ['z'] = 35.037265 },
+        reward = math.random(500,2500),
+        nameofstore = "Great Ocean Highway",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["vespuccib"] = {
+        position = { ['x'] = 1126.84399, ['y'] = -980.659484, ['z'] = 45.415672 },
+        reward = math.random(500,2500),
+        nameofstore = "Vespucci Boulevard",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["prosper"] = {
+        position = { ['x'] = -1479.24340, ['y'] = -375.064422, ['z'] = 39.1632766 },
+        reward = math.random(500,2500),
+        nameofstore = "Prosperity",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["route682"] = {
+        position = { ['x'] = 1167.57763, ['y'] = 2718.32299, ['z'] = 37.15764 },
+        reward = math.random(500,2500),
+        nameofstore = "le magasin Route 68",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["algonquin"] = {
+        position = { ['x'] = 1395.61877, ['y'] = 3612.77587, ['z'] = 34.980926 },
+        reward = math.random(500,2500),
+        nameofstore = "Algonquin Boulevard",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["north_rockford"] = {
+        position = { ['x'] = -1828.948730, ['y'] = 799.031372, ['z'] = 138.184432 },
+        reward = math.random(500,2500),
+        nameofstore = "North Rockford Drive",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["grapeseed"] = {
+        position = { ['x'] = 1707.678466, ['y'] = 4919.957519, ['z'] = 42.063678 },
+        reward = math.random(500,2500),
+        nameofstore = "Grapeseed Avenue",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    },
+    ["littleseoul_twentyfourseven"] = {
+        position = { ['x'] = -709.17022705078, ['y'] = -904.21722412109, ['z'] = 19.215591430664 },
+        reward = math.random(3000,8000),
+        nameofstore = "24/7. (Little Seoul)",
+        secondsRemaining = 200, -- seconds
+        lastrobbed = 0
+    }
+}
